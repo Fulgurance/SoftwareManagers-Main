78fc1f3f79fae2dee567b79bb9154846be863a00
fulgurance <zohran.londais@gmail.com>
2023-02-12 13:05:00 +0000

    Patched Ism installer

--- Ism/0.0.0-alpha+13022023/0.0.0-alpha+13022023.cr
+++ Ism/0.0.0-alpha+13022023/0.0.0-alpha+13022023.cr
@@ -1,5 +1,10 @@
 class Target < ISM::Software
 
+    def extract
+        super
+        moveFile("#{workDirectoryPath(false)}/ISM-Alpha-13022023","#{workDirectoryPath(false)}/Alpha-13022023")
+    end
+
     def build
         super
         runCrystalCommand(["build","Main.cr","-o","ism"],buildDirectoryPath)
@@ -9,8 +14,8 @@ class Target < ISM::Software
         super
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin")
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share")
-        moveFile("#{buildDirectoryPath(false)}ism","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin")
-        copyDirectory("#{workDirectoryPath(false)}Alpha-06022023","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ism")
+        moveFile("#{buildDirectoryPath(false)}ism","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin/ism")
+        copyDirectory("#{workDirectoryPath(false)}/Alpha-13022023","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ism")
     end
 
 end
