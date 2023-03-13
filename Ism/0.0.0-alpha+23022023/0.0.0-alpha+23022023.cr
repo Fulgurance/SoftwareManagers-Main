673a25961ae516369ef7c9e296a9a1a8b3708c5b
fulgurance <zohran.londais@gmail.com>
2023-03-13 22:20:34 +0000

    Added new Ism version

---
+++ Ism/0.0.0-alpha+23022023/0.0.0-alpha+23022023.cr
@@ -0,0 +1,21 @@
+class Target < ISM::Software
+
+    def extract
+        super
+        moveFile("#{workDirectoryPath(false)}/ISM-Alpha-23022023","#{workDirectoryPath(false)}/Alpha-23022023")
+    end
+
+    def build
+        super
+        runCrystalCommand(["build","Main.cr","-o","ism"],buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin")
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share")
+        moveFile("#{buildDirectoryPath(false)}ism","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin/ism")
+        copyDirectory("#{workDirectoryPath(false)}/Alpha-23022023","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ism")
+    end
+
+end
