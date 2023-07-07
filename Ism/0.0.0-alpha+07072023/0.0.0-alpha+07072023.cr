e253a880bf24cc713bbd7f697aafdb72d5078480
Fulgurance <zohran.londais@gmail.com>
2023-07-07 22:33:39 +0100

    Patched ism installer implementation

--- Ism/0.0.0-alpha+07072023/0.0.0-alpha+07072023.cr
+++ Ism/0.0.0-alpha+07072023/0.0.0-alpha+07072023.cr
@@ -1,11 +1,5 @@
 class Target < ISM::Software
 
-    def extract
-        super
-
-        moveFile("#{workDirectoryPath(false)}/ISM-Alpha-07072023","#{workDirectoryPath(false)}/Alpha-07072023")
-    end
-
     def build
         super
 
@@ -18,7 +12,7 @@ class Target < ISM::Software
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin")
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share")
         moveFile("#{buildDirectoryPath(false)}ism","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/bin/ism")
-        copyDirectory("#{workDirectoryPath(false)}/Alpha-07072023","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ism")
+        copyDirectory("#{workDirectoryPath(false)}/Sources","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/ism")
     end
 
 end
